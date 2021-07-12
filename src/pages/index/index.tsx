cb82d4854080d599d86871b8c18b2acdab1b41ab
taosiqi <thinklf97@gmail.com>
2021-07-12 17:02:03 +0800

    chore(All): S删除preset-ui

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,30 +1,43 @@
 import styles from './index.less';
-import { connect,Dispatch, IndexModelState, ConnectProps,useDispatch } from 'umi';
+import {
+  connect,
+  Dispatch,
+  IndexModelState,
+  ConnectProps,
+  useDispatch,
+} from 'umi';
 import React, { FC } from 'react';
 interface PageProps extends ConnectProps {
-  index:IndexModelState,
-  dispatch:Dispatch
+  index: IndexModelState;
+  dispatch: Dispatch;
 }
-const IndexPage: FC<PageProps> = (indexProps ) => {
-  let {index:{heros},dispatch}=indexProps
+const IndexPage: FC<PageProps> = (indexProps) => {
+  let {
+    index: { heros },
+  } = indexProps;
+  let dispatch = useDispatch();
   const onChange = () => {
     dispatch({
       type: 'index/save', // 如果在 model 外调用，需要添加 namespace
       payload: {
-        heros: []
+        heros: [],
       }, // 需要传递的信息
-    })
+    });
   };
   return (
     <div>
       <div onClick={onChange}>清空</div>
-      {
-        heros.map((item)=>{
-          return  <span className={styles.title} key={item.ename}>{item.cname}</span>
-        })
-      }
+      {heros.map((item) => {
+        return (
+          <span className={styles.title} key={item.ename}>
+            {item.cname}
+          </span>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default connect(({ index }: { index: IndexModelState }) => ({ index }))(IndexPage);
+export default connect(({ index }: { index: IndexModelState }) => ({ index }))(
+  IndexPage,
+);
